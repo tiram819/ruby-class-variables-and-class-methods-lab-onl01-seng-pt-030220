ab7e3bfa07277b2e2649a0771eb5a3940b2d8c8f
Tabassum Hassan <tiram819@gmail.com>
2020-03-18 03:34:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0 
-  @@artist = []
+  @@artists = []
   @@genre = []
   
   def initialize(name, artist, genre)
@@ -12,7 +12,7 @@ class Song
     @genre = genre
     @@count +=1 
     @@genre << genre
-    @@artist << artist
+    @@artists << artist
     
   end
   
