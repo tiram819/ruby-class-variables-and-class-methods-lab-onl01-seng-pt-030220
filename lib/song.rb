d3201cca2c6722eb8a836d3b9544a4182214b350
Tabassum Hassan <tiram819@gmail.com>
2020-03-18 03:38:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,3 +29,17 @@ class Song
   end
 end
 
+def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre| 
+      if genre_count[genre]
+      genre_count[genre] += 1 
+      else
+      genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+  
+  
+
